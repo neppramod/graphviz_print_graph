cd67b66402b967623f066226bfe300ea12bef383
Pramod Nepal <neppramod@gmail.com>
2021-02-19 20:20:54 -0700

    Reformat Solution.java

--- Solution.java
+++ Solution.java
@@ -1,53 +1,56 @@
 import java.util.*;
 
-
-
 public class Solution {
     public static void main(String[] args) {
-	    Solution sol = new Solution();
+        Solution sol = new Solution();
 
-		// 1. Simple Example
+        // 1. Simple Example
         GraphPrinter gp = new GraphPrinter("simple");
         gp.addln("1 -> 2");
         gp.addln("1 -> 3");
         gp.print();
 
-		// 2. ListNode
-		ListNode l = cl(1);
+        // 2. ListNode
+        ListNode l = cl(1);
         l.next = cl(2);
         l.next.next = cl(3);
         l.next.next.next = cl(4);
 
-		sol.printList(l);
+        sol.printList(l);
 
-		// 3. TreeNode
-		TreeNode tree = ct(1);
-		tree.left = ct(2);
-		tree.left.right = ct(5);
-		tree.right = ct(3);
-		tree.right.right = ct(4);
+        // 3. TreeNode
+        TreeNode tree = ct(1);
+        tree.left = ct(2);
+        tree.left.right = ct(5);
+        tree.right = ct(3);
+        tree.right.right = ct(4);
 
-		sol.printTree(tree);
-		
-		
+        sol.printTree(tree);
+
+
         // 4. Graph
         List<Integer>[] adj = new ArrayList[6];
         for (int i = 0; i < 6; i++) {
             adj[i] = new ArrayList<Integer>();
         }
-        adj[1] = new ArrayList<>(); adj[1].addAll(Arrays.asList(2, 4));
-        adj[2] =  new ArrayList<>(); adj[2].addAll(Arrays.asList(1, 3, 5));
-        adj[3] =  new ArrayList<>(); adj[3].addAll(Arrays.asList(2, 5));
-        adj[4] =  new ArrayList<>(); adj[4].addAll(Arrays.asList(1));
-        adj[5] =  new ArrayList<>(); adj[5].addAll(Arrays.asList(2, 3));
+        adj[1] = new ArrayList<>();
+        adj[1].addAll(Arrays.asList(2, 4));
+        adj[2] = new ArrayList<>();
+        adj[2].addAll(Arrays.asList(1, 3, 5));
+        adj[3] = new ArrayList<>();
+        adj[3].addAll(Arrays.asList(2, 5));
+        adj[4] = new ArrayList<>();
+        adj[4].addAll(Arrays.asList(1));
+        adj[5] = new ArrayList<>();
+        adj[5].addAll(Arrays.asList(2, 3));
 
 
         sol.printGraphAdj(adj, 6);
-	}
+    }
 
 
-	// Code for ListNode
-	static class ListNode {
+    // Code for ListNode
+    static class ListNode {
         ListNode next;
         int val;
 
@@ -56,14 +59,14 @@ public class Solution {
         }
     }
 
-	static ListNode cl(int v) {
+    static ListNode cl(int v) {
         return new ListNode(v);
-	}
+    }
 
 
-	void printList(ListNode l) {
-		GraphPrinter gp = new GraphPrinter("list");
-		
+    void printList(ListNode l) {
+        GraphPrinter gp = new GraphPrinter("list");
+
         ListNode cur = l;
 
         gp.addln("graph [rankdir=\"LR\"]");
@@ -73,13 +76,12 @@ public class Solution {
             cur = cur.next;
         }
 
-		gp.print();
+        gp.print();
     }
 
 
-	// Code for TreeNode
-	static class TreeNode
-    {
+    // Code for TreeNode
+    static class TreeNode {
         TreeNode left;
         TreeNode right;
         int val;
@@ -93,27 +95,27 @@ public class Solution {
         return new TreeNode(v);
     }
 
-	// use this variable to handle null 
-	static long nullCounter = 0;
-	
-	void printTree(TreeNode root) {
-		GraphPrinter gp = new GraphPrinter("tree");
-		nullCounter = 0;
+    // use this variable to handle null
+    static long nullCounter = 0;
 
-		// Let's use DFS to go through all nodes of the tree and add it to gp GraphPrinter object
+    void printTree(TreeNode root) {
+        GraphPrinter gp = new GraphPrinter("tree");
+        nullCounter = 0;
+
+        // Let's use DFS to go through all nodes of the tree and add it to gp GraphPrinter object
         addNodeForPrinting(root, gp);
 
-		gp.print();
+        gp.print();
     }
 
-	// DFS for traversing TreeNode and adding it to GraphPrinter object
-	// For null nodes we will add a small double circle
-	void addNodeForPrinting(TreeNode root, GraphPrinter gp) {
-		if (root == null) {
-			return;
-		}
+    // DFS for traversing TreeNode and adding it to GraphPrinter object
+    // For null nodes we will add a small double circle
+    void addNodeForPrinting(TreeNode root, GraphPrinter gp) {
+        if (root == null) {
+            return;
+        }
 
-		if (root.left != null) {
+        if (root.left != null) {
             gp.addln(String.valueOf(root.val) + " -> " + String.valueOf(root.left.val));
         } else {
             String nullStr = "n" + (nullCounter++);
@@ -132,19 +134,19 @@ public class Solution {
         }
 
         addNodeForPrinting(root.right, gp);
-	}
+    }
 
 
-	// Print Graph
-	void printGraphAdj(List[] adj, int n) {
+    // Print Graph
+    void printGraphAdj(List[] adj, int n) {
         GraphPrinter gp = new GraphPrinter("AdjGraph");
 
 
         gp.addln("concentrate=true");  // only 1 line to represent bidirectional graphs
         gp.addln("edge[arrowhead=none]");  // remove the arrowhead for graph, we don't need for bidirectional graph
 
-		gp.addln("node [margin=0 fontcolor=blue fontsize=32 width=0.5 shape=circle style=filled]");
-		gp.addln("rankdir=LR");
+        gp.addln("node [margin=0 fontcolor=blue fontsize=32 width=0.5 shape=circle style=filled]");
+        gp.addln("rankdir=LR");
 
         // code goes here
         boolean[] visited = new boolean[n];
@@ -172,5 +174,5 @@ public class Solution {
             }
         }
     }
-    
+
 }
